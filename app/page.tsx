8515ef4ae778478fd2f0297109a746c69e893b3a
meet-tola <wasiusikiru7@gmail.com>
2024-10-10 11:32:49 +0100

    fixing page routing

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { redirect } from "next/navigation";
 export default function Home() {
   const { user, isSignedIn } = useUser();
 
-  if (!user) {
+  if (user) {
     return redirect("/dashboard");
   }
 
